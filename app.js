e1a4b616ffff96e92a897d4a6e98d32b07d0d063
Salvucci Carlos <salvuccicarlos@gmail.com>
2021-05-17 13:55:41 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+/* Configuraciones */
+app.use(
+express.static(path.resolve(__dirname, 'public'))
+);
+
+/* Rutas */
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+});
+
+/* Server */
+app.listen(3005, () => console.log('Servidor corriendo en puerto 3005'));
+
+
